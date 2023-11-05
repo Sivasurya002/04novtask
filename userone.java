998e5ee58d8f044e49f687e94e74848aa8d73da3
user1 <user1@gmail.com>
2023-11-05 16:52:43 +0000

    Initial file upload

---
+++ userone.java
@@ -0,0 +1,5 @@
+class userone{
+public static void main(String[] args){
+System.out.println("Whatsapp developed");
+}
+}
---
+++ usertwo.java
@@ -0,0 +1,5 @@
+class usertwo{
+public static void main(String[] args){
+System.out.println("Instagram developed");
+}
+}
