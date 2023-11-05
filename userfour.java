2d83f8cd66812989f8e3aadfc5af3ee73c439fae
user2 <user2@gmail.com>
2023-11-05 16:59:11 +0000

    Initial files upload

---
+++ userfour.java
@@ -0,0 +1,5 @@
+class userfour{
+public static void main(String[] args){
+System.out.println("Snapshot developed");
+}
+}
---
+++ userthree.java
@@ -0,0 +1,5 @@
+class userthree{
+public static void main(String[] args){
+System.out.println("Telegram developed");
+}
+}
